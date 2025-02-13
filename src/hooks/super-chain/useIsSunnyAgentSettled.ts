d912cbae684d87ea21632dd0016e68177363fd34
anotherdev <joseluismanco37@gmail.com>
2025-02-12 19:40:07 -0500

    chore: build

--- src/hooks/super-chain/useIsSunnyAgentSettled.ts
+++ src/hooks/super-chain/useIsSunnyAgentSettled.ts
@@ -1,21 +1,21 @@
-import { useQuery } from "@tanstack/react-query"
-import useSafeInfo from "../useSafeInfo"
-import { useSafeSDK } from "../coreSDK/safeCoreSDK"
+import { useQuery } from '@tanstack/react-query'
+import useSafeInfo from '../useSafeInfo'
+import { useSafeSDK } from '../coreSDK/safeCoreSDK'
 
 function useIsSunnyAgentSettled() {
-    const { safeAddress } = useSafeInfo()
-    const safeSDK = useSafeSDK()
-    return useQuery({
-        queryKey: ['isSunnyAgentSettled', safeAddress],
-        queryFn: async () => {
-            const isEnabled = await safeSDK?.isModuleEnabled("0xde8f89B6d11fc6894C98A37458c0149787F051AE")
-            if (!isEnabled) {
-                return false
-            }
-            return true
-        },
-        enabled: !!safeAddress,
-    })
+  const { safeAddress } = useSafeInfo()
+  const safeSDK = useSafeSDK()
+  return useQuery({
+    queryKey: ['isSunnyAgentSettled', safeAddress],
+    queryFn: async () => {
+      const isEnabled = await safeSDK?.isModuleEnabled('0xde8f89B6d11fc6894C98A37458c0149787F051AE')
+      if (!isEnabled) {
+        return false
+      }
+      return true
+    },
+    enabled: !!safeAddress,
+  })
 }
 
 export default useIsSunnyAgentSettled
